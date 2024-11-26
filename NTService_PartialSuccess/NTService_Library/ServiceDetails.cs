90cd9c597bf41175bb8fb0a84c4ffee1bc0c327e
Robert Tjia <rtjia@amazon.com>
2024-11-26 15:11:36 -0500

    first commit

---
+++ NTService_PartialSuccess/NTService_Library/ServiceDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NTService_Library
+{
+    public class ServiceDetails
+    {
+        string _serviceName;
+        public ServiceDetails(string ServiceName)
+        {
+            this._serviceName = ServiceName;
+        }
+
+        public string GetServiceName()
+        {
+            return this._serviceName;
+        }
+
+        public string GetServiceDescription()
+        {
+            return "Windows NT 3.51 is a major release of the Windows NT operating system developed by Microsoft and oriented towards businesses. It is the third version of Windows NT and was released on May 30, 1995, eight months following the release of Windows NT 3.5.";
+        }
+    }
+}
---
+++ NTService_PartialSuccess/NTService_PartialSuccess/Service1.cs
@@ -0,0 +1,35 @@
+﻿using NTService_Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTService_PartialSuccess
+{
+    public partial class Service1 : ServiceBase
+    {
+        NTService_Library.ServiceDetails _serviceDetails = null;
+        
+        public Service1()
+        {
+            InitializeComponent();
+            _serviceDetails = new ServiceDetails("NT_Serivce");
+            EventLog.CreateEventSource(_serviceDetails.GetServiceName(), "Application");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry(_serviceDetails.GetServiceName(), "Started", EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry(_serviceDetails.GetServiceName(), "Stopped", EventLogEntryType.Information);
+        }
+    }
+}
