1ef9fe5d872bf9e2054cd41bfb6089a044b06224
root <root@ip-172-31-52-5.ec2.internal>
2024-11-26 20:27:19 +0000

    qtransform commit for jobId 72889f4a-ccd2-44c5-88ff-a3ff3e3d76c8

--- NTService_PartialSuccess/NTService_PartialSuccess/obj/Debug/net8.0/NTService_PartialSuccess.AssemblyInfo.cs
+++ NTService_PartialSuccess/NTService_PartialSuccess/obj/Debug/net8.0/NTService_PartialSuccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTService_PartialSuccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cd9c597bf41175bb8fb0a84c4ffee1bc0c327e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e30bddaddd445c153d2f74122aad3c2e2083d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTService_PartialSuccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTService_PartialSuccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
